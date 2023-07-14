d299ac8b7622a1a4956985abbe3d5852a5fb5fee
Miaokeda <miaokeda.mkd@alibaba-inc.com>
2023-07-14 17:15:04 +0800

    Feat: Mesh

--- include/opengl/mesh.hpp
+++ include/opengl/mesh.hpp
@@ -1,15 +1,69 @@
 #pragma once
 #include "glad/glad.h"
-class VertexObject {
-private:
-  GLuint vao;
+#include "glm/glm.hpp"
+#include "shader.hpp"
+#include <memory>
+#include <string>
+#include <vector>
 
-public:
+const int MAX_BONE_INFLUENCE = 4;
+
+struct SimpleVertex {
+  glm::vec3 position;
+  glm::vec3 color;
+  glm::vec3 normal;
+  glm::vec2 texCoords;
 };
 
-class BufferObject {
-private:
-  GLuint vbo;
+struct Vertex {
+  glm::vec3 position;
+  glm::vec3 normal;
+  glm::vec2 texCoords;
+  glm::vec3 tangent;
+  glm::vec3 bitangent;
+};
+
+struct Texture {
+  GLuint id;
+  std::string type;
+  std::string path;
+};
+
+class SimpleMesh {
+protected:
+  GLuint m_VAO;
+  GLuint m_VBO;
+  GLuint m_EBO;
+  std::vector<SimpleVertex> m_vertices;
+  std::vector<GLuint> m_indices;
+
+  void setup();
 
 public:
+  SimpleMesh(std::vector<SimpleVertex> vertices, std::vector<GLuint> indices);
+  std::shared_ptr<SimpleMesh> createMesh(std::vector<Vertex> vertices,
+                                         std::vector<GLuint> indices);
+  ~SimpleMesh();
+  inline void draw(Shader &shader);
+};
+
+class Mesh {
+protected:
+  GLuint m_VAO;
+  GLuint m_VBO;
+  GLuint m_EBO;
+  std::vector<Vertex> m_vertices;
+  std::vector<GLuint> m_indices;
+  std::vector<Texture> m_textures;
+
+  void setup();
+
+public:
+  Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices,
+       std::vector<Texture> textures);
+  std::shared_ptr<Mesh> createMesh(std::vector<Vertex> vertices,
+                                   std::vector<GLuint> indices,
+                                   std::vector<Texture> textures);
+  ~Mesh();
+  inline void draw(Shader &shader);
 };
---
+++ src/mesh.cpp
@@ -0,0 +1,104 @@
+#include "opengl/mesh.hpp"
+
+std::shared_ptr<Mesh> Mesh::createMesh(std::vector<Vertex> vertices,
+                                       std::vector<GLuint> indices,
+                                       std::vector<Texture> textures) {
+  return std::make_shared<Mesh>(vertices, indices, textures);
+}
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices,
+           std::vector<Texture> textures)
+    : m_vertices(vertices), m_indices(indices), m_textures(textures) {
+  setup();
+}
+
+Mesh::~Mesh() {}
+
+void Mesh::setup() {
+  glGenVertexArrays(1, &m_VAO);
+  glGenBuffers(1, &m_VBO);
+  glGenBuffers(1, &m_EBO);
+
+  glBindVertexArray(m_VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+
+  glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(Vertex),
+               &m_vertices[0], GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_indices.size() * sizeof(GLuint),
+               &m_indices[0], GL_STATIC_DRAW);
+
+  // position
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid *)0);
+
+  // normal
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (GLvoid *)offsetof(Vertex, normal));
+
+  // texCoords
+  glEnableVertexAttribArray(2);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (GLvoid *)offsetof(Vertex, texCoords));
+
+  // tangent
+  glEnableVertexAttribArray(3);
+  glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (GLvoid *)offsetof(Vertex, tangent));
+
+  // bitangent
+  glEnableVertexAttribArray(4);
+  glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (GLvoid *)offsetof(Vertex, bitangent));
+}
+
+inline void Mesh::draw(Shader &shader) {
+  shader.use();
+  glBindVertexArray(m_VAO);
+  glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, 0);
+  glBindVertexArray(0);
+}
+
+// std::shared_ptr<Mesh> TextureMesh::createMesh(std::vector<Vertex> vertices,
+//                                               std::vector<GLuint> indices,
+//                                               std::vector<Texture> textures)
+//                                               {
+//   return std::make_shared<TextureMesh>(vertices, indices, textures);
+// }
+
+// TextureMesh::TextureMesh(std::vector<Vertex> vertices,
+//                          std::vector<GLuint> indices,
+//                          std::vector<Texture> textures)
+//     : Mesh(vertices, indices), m_textures(textures) {
+//   setup();
+// }
+
+// inline void TextureMesh::draw(Shader &shader) {
+//   GLuint diffuseNr = 1;
+//   GLuint specularNr = 1;
+//   GLuint normalNr = 1;
+//   GLuint heightNr = 1;
+//   shader.use();
+//   for (GLuint i = 0; i < m_textures.size(); i++) {
+//     glActiveTexture(GL_TEXTURE0 + i);
+//     std::string name = m_textures[i].type;
+//     std::string number;
+//     if (name == "texture_diffuse")
+//       number = std::to_string(diffuseNr++);
+//     else if (name == "texture_specular")
+//       number = std::to_string(specularNr++);
+//     else if (name == "texture_normal")
+//       number = std::to_string(normalNr++);
+//     else if (name == "texture_height")
+//       number = std::to_string(heightNr++);
+//     shader.setInt((name + number).c_str(), i);
+//     glBindTexture(GL_TEXTURE_2D, m_textures[i].id);
+//   }
+//   glActiveTexture(GL_TEXTURE0);
+
+//   glBindVertexArray(m_VAO);
+//   glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, 0);
+//   glBindVertexArray(0);
+// }
