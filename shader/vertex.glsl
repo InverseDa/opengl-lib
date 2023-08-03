3de4a58b70b5d910bc129b632c566d089be94a8b
Miaokeda <miaokeda.mkd@alibaba-inc.com>
2023-08-03 17:44:24 +0800

    feat: CMakeList Change

--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -7,8 +7,10 @@ out vec3 fragment;
 
 uniform mat4 projection;
 uniform mat4 model;
+uniform mat4 view;
 
 void main() {
-    gl_Position = vec4(pos, 1.0f);
+    gl_Position = projection * view * model * vec4(pos, 1.0f);
+    // gl_Position = vec4(pos, 1.0f);
     fragment = color;
 }
